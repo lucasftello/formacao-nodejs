80d50acb41479eb3ea3990ffc9c38b78724bc9a7
Lucas Tello <lucastelloweb@gmail.com>
2023-04-09 16:06:32 -0300

    Módulo 01

---
+++ 01-introducao/app.js
@@ -0,0 +1,20 @@
+const { nome, soma, sub, mult, div } = require('./calculadora');
+// const calculadora = require('./calculadora');
+
+// console.log(calculadora.nome);
+console.log(nome);
+console.log(soma(40, 20));
+console.log(sub(40, 20));
+console.log(mult(10, 10));
+console.log(div(60, 10));
+
+// const mostrarSite = true;
+// const site = 'lucastello.dev';
+
+// console.log('Hello world');
+// console.log('Meu nome é Lucas');
+// console.log('E eu estou aprendendo Node.js com o Guia do programador');
+
+// if (mostrarSite) {
+//   console.log('Meu site é ' + site);
+// }
---
+++ 01-introducao/calculadora.js
@@ -0,0 +1,19 @@
+const nome = 'Minha calculadora v1';
+
+function soma(a, b) {
+  return a + b;
+}
+
+function sub(a, b) {
+  return a - b;
+}
+
+function mult(a, b) {
+  return a * b;
+}
+
+function div(a, b) {
+  return a / b;
+}
+
+module.exports = { nome, soma, sub, mult, div };
